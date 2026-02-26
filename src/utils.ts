2eb550a0fded93f818000a04a5fb19f9ad6badd8
Alexey <antiporovi4@gmail.com>
2026-02-26 10:25:20 +0300

    update decodeHtmlEntities &nbsp;

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,8 @@ export function decodeHtmlEntities(text: string): string {
     .replace(/&lt;/g, '<')
     .replace(/&gt;/g, '>')
     .replace(/&quot;/g, '"')
-    .replace(/&#39;/g, "'");
+    .replace(/&#39;/g, "'")
+    .replace(/&nbsp;/g, ' ');
 }
 
 export function normalizeSpaces(text: string): string {
